7c90f3713de0b691b2b0a2d9cbfc6c367afdf480
ATwhiteIIIQwQ <atwhite0527@163.com>
2025-03-24 08:42:27 +0800

    imported project

---
+++ src/main/java/com/atotwhite/Menu.java
@@ -0,0 +1,15 @@
+package com.atotwhite;
+
+public class Menu {
+    static void show() {
+        System.out.println("-------学生信息管理系统-------");
+        System.out.println("***    1.添加学生信息     ***");
+        System.out.println("***   2.查看全部学生信息   ***");
+        System.out.println("***      3.信息修改       ***");
+        System.out.println("***      4.信息删除       ***");
+        System.out.println("***      5.信息查询       ***");
+        System.out.println("***        6.退出        ***");
+        System.out.println("---------------------------");
+        System.out.print("请选择操作（输入数字）：");
+    }
+}
